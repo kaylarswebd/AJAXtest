b2f92b410fdf0f8ff812fd43240b32d246056db5
kaylarswebd <54419990+kaylarswebd@users.noreply.github.com>
2020-01-16 10:52:33 -0600

    minor fix

--- java/script.js
+++ java/script.js
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
 var request = new XMLHttpRequest();
 request.open('GET','data.txt',false);
 request.send();
 console.log(request);
 document.getElementById('demo').innerHTML = request.responseText;
-=======
-var request = new XMLHttpRequest();
-request.open('GET','data.txt',false);
-request.send();
-console.log(request);
-document.getElementById('demo').innerHTML = request.responseText;
->>>>>>> ef6619a5f26a83a7d2d0ff92baea29ebdf1a123a
