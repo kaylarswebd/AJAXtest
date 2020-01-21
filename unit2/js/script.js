cb74c33fa15ec69519bb868e39638f5fc0129edf
kaylarswebd <54419990+kaylarswebd@users.noreply.github.com>
2020-01-21 10:55:18 -0600

    added folder and changed content

---
+++ unit2/js/script.js
@@ -0,0 +1,13 @@
+for (var i = 0; i < 100; i++) {
+    var request = new XMLHttpRequest();
+    request.open('GET', 'data.txt', false);
+
+    request.readystatechange = function () {
+        // document.getElementById('demo').innerHTML = request.responseText;
+        if ((request.status === 200) && (request.readyState === 4)) {
+            console.log(request);
+            document.writeln(request.responseText);
+        }
+    }
+    request.send();
+}
